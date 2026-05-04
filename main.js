85a976e41cfe8f2d8c82e675a6f30523161fdaeb
6drge <102768483+6drge@users.noreply.github.com>
2026-05-04 10:25:57 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+const plr = document.getElementById("player")
+document.addEventListener("keydown", movement)
+
+function getTranslateXY(element) {
+    const style = window.getComputedStyle(element)
+    const matrix = new DOMMatrixReadOnly(style.transform)
+    return {
+        translateX: matrix.m41,
+        translateY: matrix.m42
+    }
+}
+
+function movement(key) {
+  if (key == "w") {
+    let x1,y1 = getTranslateXY(plr)
+    let up = x1+0.02
+    plr.style.transform = "translate(x1, up)";
+  }
+}
+
+function isCollide(a, b) {
+    var aRect = a.getBoundingClientRect();
+    var bRect = b.getBoundingClientRect();
+
+    return !(
+        ((aRect.top + aRect.height) < (bRect.top)) ||
+        (aRect.top > (bRect.top + bRect.height)) ||
+        ((aRect.left + aRect.width) < bRect.left) ||
+        (aRect.left > (bRect.left + bRect.width))
+    );
+}
