d53b3cd5d8ea864028e9ec83b71392cea3aaa040
6drge <102768483+6drge@users.noreply.github.com>
2026-05-04 10:58:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function movement(e) {
   if (e.key === "w") {
     let x1,y1 = getTranslateXY(plr);
     let up = x1+1;
-    plr.style.transform = "translate(${x1}px, ${up}px)";
+    plr.style.transform = "translate(${x1}px, ${up}px);";
   }
 }
 
