ee0e4961117261177c78a243b9a25511aa2f4a1b
6drge <102768483+6drge@users.noreply.github.com>
2026-05-05 09:00:14 -0500

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,6 +1,6 @@
 onmessage = (e) => {
-  let plr = e[0]
-  let keys = e[1]
+  let plr = e[0];
+  let keys = e[1];
   if (keys.includes("s")) {
     plr.style.transform += "translate(0px, 10px)";
       console.log("2nd");
