f5fe0163dba3e7abc409628167057e5043641aec
6drge <102768483+6drge@users.noreply.github.com>
2026-05-05 08:39:10 -0500

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,3 +1,20 @@
-onmessage = (key) => {
-
+onmessage = (e) => {
+  let plr = e[0]
+  let key = e[1]
+  if (key == "s") {
+    plr.style.transform += "translate(0px, 10px)";
+      console.log("2nd");
+  }
+  if (key == "d") {
+    plr.style.transform += "translate(10px, 0px)";
+      console.log("2nd");
+  }
+  if (key == "w") {
+    plr.style.transform += "translate(0px, -10px)";
+      console.log("2nd");
+  }
+  if (key == "a") {
+    plr.style.transform += "translate(-10px, 0px)";
+      console.log("2nd");
+  }
 };
