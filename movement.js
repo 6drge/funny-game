2b01e5ad9c8bc2d45f2f4c7125e14d6a7ac5d164
6drge <102768483+6drge@users.noreply.github.com>
2026-05-06 08:22:53 -0500

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,15 +1,17 @@
 onmessage = (e) => {
   let keys = e.data;
+  let transform = "translate(0px, 0px)";
   if (keys.includes("s")) {
-      postMessage("translate(0px, 10px)");
+      transform += "translate(0px, 10px)";
   }
   if (keys.includes("d")) {
-      postMessage("translate(10px, 0px)");
+      transform += "translate(10px, 0px)";
   }
   if (keys.includes("w")) {
-      postMessage("translate(0px, -10px)");
+      transform += "translate(0px, -10px)";
   }
   if (keys.includes("a")) {
-      postMessage("translate(-10px, 0px)");
+      transform += "translate(-10px, 0px)"
   }
+  postMessage(transform);
 };
